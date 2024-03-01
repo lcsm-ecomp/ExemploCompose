944374b1fd66bfd024f690c4cdaff42d6ae92b24
lcsm-ecomp <32273415+lcsm-ecomp@users.noreply.github.com>
2024-03-01 08:37:12 -0300

    Add files via upload

--- src/desktopMain/kotlin/main.kt
+++ src/desktopMain/kotlin/main.kt
@@ -6,33 +6,59 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.*
 import androidx.compose.ui.unit.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-
-
-
-var nome by mutableStateOf("Luis")
-var mostraBoasVindas by mutableStateOf(false)
-
-@Composable
-fun makeApp() {
-        Column( Modifier.fillMaxWidth().padding(15.dp), horizontalAlignment = Alignment.CenterHorizontally ) {
-          println("Desenhando UI")
-          TextField(value=nome, label={ Text("Qual o seu nome?")} , onValueChange = {nome = it})
-          Button( onClick = { mostraBoasVindas = !mostraBoasVindas; println("Botão Pressionado") }) { 
-            Text("Pressione" + nome) 
-            println("Desenhando Botao")
-          }
-          if (mostraBoasVindas) Text("Ola " + nome)
-        }
-
-}
-
-
+import androidx.compose.foundation.background
+import androidx.compose.animation.*
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "Ola Mundo") {
-        makeApp()
+    var tela by mutableStateOf(0)
+    var nome by mutableStateOf("Luis")
+    var quantidade by mutableStateOf(0)
+    var mostraSaudacao by mutableStateOf(true)
+
+
+    Window(onCloseRequest = ::exitApplication, title = "Ola Compose") {
+         Column(Modifier.fillMaxWidth().padding(60.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+           Text("Minha Primeira Aplicação Compose")
+           AnimatedVisibility (tela==0) {
+              Column(Modifier.fillMaxWidth().padding(60.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+
+              Text("Ola Pessoal")
+              Button( onClick={ tela=1 }) {
+                Text("Iniciar Aplicacao")
+              }   
+              }
+           } 
+           AnimatedVisibility(tela==1) {
+              Column(Modifier.fillMaxWidth().padding(60.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+
+              TextField(value=nome, label={Text("Digite o seu Nome: ")}, onValueChange={ novoNome -> 
+                  println("O usuario modificou o seu nome para ${novoNome}")
+                  nome = novoNome
+              })
+              Text("")
+              Button(onClick={ 
+                println("O usuario apertou o botao") 
+                quantidade = quantidade + 1
+                mostraSaudacao = !mostraSaudacao
+              }) {
+                  println("UI Botão esta sendo construida")
+                  Text("$nome\napertou este botão\n$quantidade vezes")
+                  
+              }
+              Text("")
+              AnimatedVisibility(mostraSaudacao) {
+                  Text("Bom dia $nome")
+              }
+
+              Button(onClick= { tela=0 }) {
+                Text("Sair da Aplicação")
+              }
+           }
+           }
+         }
     }
 }
 
